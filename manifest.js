7a95464c1617e1ea32def67b75e4ab8e5de5ebe5
Przemyslaw Szelenberger <przemek.szelenberger@performgroup.com>
2014-05-27 16:20:59 +0200

    Minor changes

--- manifest.js
+++ manifest.js
@@ -4,22 +4,22 @@ var baseurl = loc.substring(0,loc.lastIndexOf('/'));
 function getManifest() {
   return {
     // currently required
-    "name": "Social Sidebar Example",
+    "name": "Goal Mozilla Sidebar Demo",
     "origin": baseurl,
     // icons from http://findicons.com/icon/158311/firefox?id=356182 by ipapun
     "iconURL": baseurl+"/firefox16.png",
     "icon32URL": baseurl+"/firefox32.png",
     "icon64URL": baseurl+"/firefox64.png",
-  
+
     // at least one of these must be defined
     "sidebarURL": baseurl+"/sidebar.htm",
-  
+
     // should be available for display purposes
     "description": "A simple example of a social sidebar",
-    "author": "Shane Caraveo, Mozilla",
+    "author": "Shane Caraveo, Mozilla && Przemysław Szelenberger, Perform Media",
     "homepageURL": "https://github.com/mixedpuppy/socialapi-sidebar/",
-  
+
     // optional
     "version": 1
-  }
+  };
 }
