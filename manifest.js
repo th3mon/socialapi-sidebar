ef3fdd08722f6510f151d3f5d9e68557edb44699
Przemyslaw Szelenberger <przemek.szelenberger@performgroup.com>
2014-06-05 13:54:48 +0200

    CHANGE: Manifest data

--- manifest.js
+++ manifest.js
@@ -4,7 +4,7 @@ var baseurl = loc.substring(0,loc.lastIndexOf('/'));
 function getManifest() {
   return {
     // currently required
-    "name": "Goal Mozilla Sidebar Demo",
+    "name": "Goal.com",
     "origin": baseurl,
     // icons from http://findicons.com/icon/158311/firefox?id=356182 by ipapun
     "iconURL": baseurl+"/firefox16.png",
@@ -15,8 +15,8 @@ function getManifest() {
     "sidebarURL": "http://mozilla.qa.goal.com/mozilla-sidebar",
 
     // should be available for display purposes
-    "description": "A simple example of a social sidebar",
-    "author": "Shane Caraveo, Mozilla && Przemysław Szelenberger, Perform Media",
+    "description": "Goal.com social sidebar",
+    "author": "PERFORM Group",
     // "homepageURL": "https://github.com/mixedpuppy/socialapi-sidebar/",
 
     // optional
